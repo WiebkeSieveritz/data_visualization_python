fef1aafa262e34d42a9798cc483791185d400894
WiebkeSieveritz <w.sieveritz@gmx.de>
2024-05-16 04:46:32 +0200

    Upload raw vision

---
+++ graphs.py
@@ -0,0 +1,298 @@
+import main
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from matplotlib.ticker import FuncFormatter, ScalarFormatter
+
+
+# creation of new DataFrame of all means:
+data = {
+    'Gender': ['Male', 'Female', 'Total'],
+    'Arithmetic mean': [main.arith_mean_male, main.arith_mean_female, main.arith_mean_total],
+    'Median': [main.median_male, main.median_female, main.median_total],
+    'Trimmed mean': [main.trimmed_mean_male, main.trimmed_mean_female, main.trimmed_mean_total]
+}
+df_means = pd.DataFrame(data)
+df_melted = pd.melt(df_means, id_vars='Gender', var_name='Means', value_name='Value')
+
+education_level = ["Bachelor's", "Master's", "PhD"]
+education_level_s = ["PhD", "Master's", "Bachelor's"]
+labels_legend = ['Male', 'Female', 'Total']
+means = ['Arithmetic mean', 'Median', 'Trimmed mean']
+
+arith_mean_male_el = [main.arith_mean_bachelor_male, main.arith_mean_master_male, main.arith_mean_phd_male]
+arith_mean_female_el = [main.arith_mean_bachelor_female, main.arith_mean_master_female, main.arith_mean_phd_female]
+arith_mean_total_el = [main.arith_mean_bachelor_total, main.arith_mean_master_total, main.arith_mean_phd_total]
+
+median_male_el = [main.median_bachelor_male, main.median_master_male, main.median_phd_male]
+median_female_el = [main.median_bachelor_female, main.median_master_female, main.median_phd_female]
+median_total_el = [main.median_bachelor_total, main.median_master_total, main.median_phd_total]
+
+trimmed_mean_male_el = [main.trimmed_mean_bachelor_male, main.trimmed_mean_master_male, main.trimmed_mean_phd_male]
+trimmed_mean_female_el = [main.trimmed_mean_bachelor_female, main.trimmed_mean_master_female, main.trimmed_mean_phd_female]
+trimmed_mean_total_el = [main.trimmed_mean_bachelor_total, main.trimmed_mean_master_total, main.trimmed_mean_phd_total]
+
+avg_less = round((1-(main.arith_mean_female/main.arith_mean_male))*100, 2)
+
+
+# plot creation of means
+fig1 = plt.figure(figsize=(10, 6))
+colors = sns.color_palette('muted')
+colors[1] = 'lightcoral'
+colors_adjusted = sns.color_palette('muted')
+colors_adjusted[0] = 'lightgrey'
+colors_adjusted[1] = 'lightcoral'
+colors_adjusted[2] = 'grey'
+
+ax1 = fig1.add_subplot(2, 1, 1)
+barplot = sns.barplot(x='Means', y='Value', hue='Gender', data=df_melted, palette=colors_adjusted, ax=ax1)
+
+ax2 = fig1.add_subplot(2, 3, 4)
+ax2.bar(education_level, arith_mean_male_el, color=colors_adjusted[0], label="Bachelor's")
+ax2.bar(education_level, arith_mean_female_el, bottom=arith_mean_male_el, color=colors_adjusted[1], label="Master's")
+ax2.bar(education_level, arith_mean_total_el, bottom=[sum(x) for x in zip(arith_mean_male_el, arith_mean_female_el)],
+        color=colors_adjusted[2], label="PhD")
+
+ax3 = fig1.add_subplot(2, 3, 5, sharey=ax2)
+ax3.bar(education_level, median_male_el, color=colors_adjusted[0], label="Bachelor's")
+ax3.bar(education_level, median_female_el, bottom=median_male_el, color=colors_adjusted[1], label="Master's")
+ax3.bar(education_level, median_total_el, bottom=[sum(x) for x in zip(median_male_el, median_female_el)],
+        color=colors_adjusted[2], label="PhD")
+
+ax4 = fig1.add_subplot(2, 3, 6, sharey=ax2)
+ax4.bar(education_level, trimmed_mean_male_el, color=colors_adjusted[0], label="Bachelor's")
+ax4.bar(education_level, trimmed_mean_female_el, bottom=trimmed_mean_male_el, color=colors_adjusted[1], label="Master's")
+ax4.bar(education_level, trimmed_mean_total_el, bottom=[sum(x) for x in zip(trimmed_mean_male_el, trimmed_mean_female_el)],
+        color=colors_adjusted[2], label="PhD")
+
+
+def thousand_separator(x, pos):
+    return '{:,.0f}'.format(x)
+
+
+ax1.set_title('Different salary means by gender', fontsize=14, y=1.2, fontweight='bold')
+plt.suptitle(f'Women earn {avg_less}% less', fontsize=12, y=0.94)
+
+ax1.set_xlabel('')
+ax1.set_xticklabels(labels=means, fontsize=9, fontweight='bold')
+ax1.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+ax2.set_xlabel('Arithmetic mean', fontsize=9, fontweight='bold')
+ax2.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+ax3.set_xlabel('Median', fontsize=9, fontweight='bold')
+ax4.set_xlabel('Trimmed mean', fontsize=9, fontweight='bold')
+
+formatter = FuncFormatter(thousand_separator)
+ax1.yaxis.set_major_formatter(formatter)
+ax2.yaxis.set_major_formatter(formatter)
+ax3.yaxis.set_visible(False)
+ax4.yaxis.set_visible(False)
+
+ax1.legend(title='', labels=['Total', 'Male', 'Female'])
+handles, labels = barplot.get_legend_handles_labels()
+ax1.legend(handles[0:], labels[0:], title='', loc='upper right', fontsize='small')
+ax4.legend(labels=labels_legend, loc='upper right', fontsize='small')
+
+for ax in [ax1, ax2, ax3, ax4]:
+    ax.set_facecolor('#F5F5F5')
+for ax in [ax2, ax3, ax4]:
+    ax.tick_params(axis='both', which='major', labelsize=8)
+for ax in [ax1]:
+    ax.tick_params(axis='y', which='major', labelsize=8)
+for ax in [ax1, ax2]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+for ax in [ax3, ax4]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+plt.show()
+
+
+# plot creation of probability density function
+colors = sns.color_palette('muted')
+yellow_muted = sns.desaturate('#FFFF00', 0.7)
+orange_muted = sns.desaturate('#FFA500', 0.7)
+turquoise_muted = sns.desaturate('#00CED1', 0.7)
+degree_colors = [yellow_muted, orange_muted, turquoise_muted]
+palette_g = {'Male': sns.color_palette("muted")[0], 'Female': 'lightcoral'}
+male_color = colors[0]
+female_color_grey = 'lightgrey'
+total_color = 'grey'
+
+fig2a, (ax1, ax2) = plt. subplots(1, 2, figsize=(10, 6))
+
+sns.violinplot(data=main.salary_data, x='Gender', y='Salary', ax=ax1, palette={'Male': male_color,
+                                                                               'Female': female_color_grey})
+sns.violinplot(data=main.salary_data, x=(len(main.salary_data['Salary'])), y='Salary', ax=ax1, color=total_color,)
+sns.histplot(data=main.salary_data, x='Salary', hue='Education Level', multiple='stack', ax=ax2, palette=degree_colors)
+
+plt.suptitle('Distribution of salaries', fontsize=14, fontweight='bold')
+ax1.set_title('Men have a higher salary range', fontsize=12)
+ax2.set_title('A PhD degree results in a higher salary', fontsize=12)
+
+ax1.set_xticklabels(['Male', 'Female', 'Total'])
+ax1.set_xlabel('Gender', fontsize=9, fontweight='bold')
+ax1.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+ax2.set_xlabel('Salary ($)', fontsize=9, fontweight='bold')
+ax2.set_ylabel('Amount', fontsize=9, fontweight='bold')
+
+ax2.legend(labels=education_level_s, loc='upper right', fontsize='small', title=None)
+
+for ax in [ax1, ax2]:
+    ax.xaxis.set_major_formatter(formatter)
+    ax.yaxis.set_major_formatter(formatter)
+
+#ax1.text(0.0, -0.15, f'Standard Error male: {main.standard_error_male}\nStandard Error female: {main.standard_error_female}\nStandard Error total: {main.standard_error_total}', transform=ax1.transAxes, fontsize=8)
+ax1.text(0.25, -0.10, f"Share of male: {main.share_male}%  -  Share of female: {main.share_female}%", transform=ax1.transAxes, fontsize=8)
+ax2.text(0.08, -0.10, f"Share of Bachelor's: {main.share_bachelor}%  -  Share of Master's: {main.share_master}%  -  Share of PhD: {main.share_phd}%", transform=ax2.transAxes, fontsize=8)
+
+for ax in [ax1, ax2]:
+    ax.set_facecolor('#F5F5F5')
+for ax in [ax1, ax2]:
+    ax.tick_params(axis='both', which='major', labelsize=8)
+for ax in [ax1, ax2]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+plt.show()
+
+
+fig2b, (ax3, ax4) = plt. subplots(1, 2, figsize=(10, 6))
+
+sns.kdeplot(data=main.salary_data, x='Salary', hue='Gender', fill=True, ax=ax3, palette=palette_g)
+sns.kdeplot(data=main.salary_data, x='Salary', hue='Education Level', fill=True, ax=ax4, palette=degree_colors)
+
+plt.suptitle('Overlapping density plot of salary', fontsize=14, fontweight='bold')
+ax3.set_title('Salary for men and women: Mostly in agreement', fontsize=12)
+ax4.set_title('Income largely depends on the degree', fontsize=12)
+
+ax3.set_xlabel('Salary ($)', fontsize=9, fontweight='bold')
+ax3.set_ylabel('Density', fontsize=9, fontweight='bold')
+ax4.set_xlabel('Salary ($)', fontsize=9, fontweight='bold')
+ax4.set_ylabel('Density', fontsize=9, fontweight='bold')
+
+for ax in [ax3, ax4]:
+    ax.xaxis.set_major_formatter(formatter)
+    ax.yaxis.set_major_formatter(formatter)
+
+for ax in [ax3, ax4]:
+    ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=False))
+    ax.yaxis.get_major_formatter().set_scientific(False)
+
+ax3.legend(labels=labels_legend, loc='upper right', fontsize='small', title=None)
+ax4.legend(labels=education_level_s, loc='upper right', fontsize='small', title=None)
+
+for ax in [ax3, ax4]:
+    ax.set_facecolor('#F5F5F5')
+for ax in [ax3, ax4]:
+    ax.tick_params(axis='both', which='major', labelsize=8)
+for ax in [ax3, ax4]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+plt.show()
+
+
+# plot creation confidence intervall and interquartile range incl. distribution
+female_color = 'lightcoral'
+fig3, (ax1, ax2) = plt. subplots(1, 2, figsize=(10, 6))
+
+sns.histplot(main.salaries, kde=True, color=colors[2], ax=ax1)
+ax1.axvline(main.confidence_intervall_total[0], linestyle='--', color='darkred', label='Lower CI - 5%')
+ax1.axvline(main.confidence_intervall_total[1], linestyle='--', color='darkred', label='Upper CI - 95%')
+ax1.axvline(main.q1_total, linestyle='-.', color='black', label='Lower IQR - 25%')
+ax1.axvline(main.q3_total, linestyle='-.', color='black', label='Upper IQR - 75%')
+sns.boxplot(data=main.salary_data, x='Gender', y='Salary', ax=ax2, palette={'Male': male_color, 'Female': female_color})
+sns.swarmplot(data=main.salary_data, x='Gender', y='Salary', ax=ax2, color='black', alpha=0.5)
+# sns.boxplot(data=main.salary_data, y='Salary', ax=ax2)
+
+plt.suptitle('Distribution of salaries', fontsize=14, fontweight='bold')
+ax1.set_title(f'Salaries have a wide range,\nbut the mean is more likely {main.arith_mean_total}$', fontsize=12)
+ax2.set_title('Men have larger outliers than women', fontsize=12,)
+
+ax1.set_xlabel('Salary ($)', fontsize=9, fontweight='bold')
+ax1.set_ylabel('Amount', fontsize=9, fontweight='bold')
+ax2.set_xlabel('Gender', fontsize=9, fontweight='bold')
+ax2.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+
+ax1.xaxis.set_major_formatter(formatter)
+ax2.yaxis.set_major_formatter(formatter)
+
+for ax in [ax1, ax2]:
+    ax.set_facecolor('#F5F5F5')
+for ax in [ax1, ax2]:
+    ax.tick_params(axis='both', which='major', labelsize=8)
+for ax in [ax1, ax2]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+ax1.legend(fontsize='small')
+
+plt.show()
+
+
+# plot creation of covariance and correlation
+from matplotlib.patches import Ellipse
+
+# Covariance matrix
+cov_matrix = np.array([[np.var(main.experience), main.covariance_exp_sal],
+                       [main.covariance_exp_sal, np.var(main.salaries)]])
+
+# Eigenvalues and eigenvectors of covariance matrix
+eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+angle = np.degrees(np.arctan2(*eigenvectors[:, 0][::-1]))
+
+# Covariance ellipse
+ellipse = Ellipse(xy=(main.arith_mean_experience, main.arith_mean_total),
+                  width=np.sqrt(5.991 * eigenvalues[0]),
+                  height=np.sqrt(5.991 * eigenvalues[1]),
+                  angle=angle,
+                  edgecolor='r',
+                  fc='None')
+
+# Plot
+fig4, (ax1, ax2) = plt. subplots(1, 2, figsize=(10, 6))
+
+ax1.add_patch(ellipse)
+ax1.scatter(main.experience, main.salaries, alpha=0.5, c=[palette_g[gender] for gender in main.salary_data['Gender']])
+ax2.scatter(main.experience, main.salaries, alpha=0.5, c=[palette_g[gender] for gender in main.salary_data['Gender']])
+slope, intercept = np.polyfit(main.experience, main.salaries, 1)
+plt.plot(main.experience, slope * np.array(main.experience) + intercept, color='red', label='Linear Fit')
+
+plt.suptitle('Connection between years of experience and salary', fontsize=14, fontweight='bold')
+ax1.set_title('High covariance', fontsize=12)
+ax2.set_title('High correlation', fontsize=12)
+
+ax1.set_xlabel('Years of Experience', fontsize=9, fontweight='bold')
+ax2.set_xlabel('Years of Experience', fontsize=9, fontweight='bold')
+ax1.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+ax2.set_ylabel('Salary ($)', fontsize=9, fontweight='bold')
+ax1.yaxis.set_major_formatter(formatter)
+ax2.yaxis.set_major_formatter(formatter)
+
+legend_elements = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, label=label)
+                   for label, color in palette_g.items()]
+ax2.legend(handles=legend_elements, loc='upper right')
+
+ax1.grid(True)
+ax2.grid(True)
+
+for ax in [ax1, ax2]:
+    ax.set_facecolor('#F5F5F5')
+for ax in [ax1, ax2]:
+    ax.tick_params(axis='both', which='major', labelsize=8)
+for ax in [ax1, ax2]:
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+ax1.text(0.08, -0.10, f'Variance male: {main.variance_male}  -  female: {main.variance_female}  -  total: '
+                     f'{main.variance_total}', transform=ax1.transAxes, fontsize=8)
+ax1.text(0.1, -0.13, f'Standard deviation male: {main.standard_deviation_male}  -  female: '
+                     f'{main.standard_deviation_female}  -  total: {main.standard_deviation_total}',
+         transform=ax1.transAxes, fontsize=8)
+
+plt.show()
---
+++ main.py
@@ -0,0 +1,276 @@
+import pandas as pd
+import numpy as np
+import statistics
+
+
+# import data set
+path = (r'C:\Users\wsiev\OneDrive\Desktop\IU - Data Analyst Python\3. Modul - Explorative Data Analysis and Visualization\Task - neu\Salary Data.csv')
+salary_data = pd.read_csv(path)
+salary_data = salary_data.dropna()
+
+
+# Shares of total
+share_bachelor = round(len(salary_data[salary_data['Education Level'] == "Bachelor's"])/len(salary_data['Education Level'])*100, 2)
+share_master = round(len(salary_data[salary_data['Education Level'] == "Master's"])/len(salary_data['Education Level'])*100, 2)
+share_phd = round(len(salary_data[salary_data['Education Level'] == "PhD"])/len(salary_data['Education Level'])*100,2)
+share_male = round(len(salary_data[salary_data['Gender'] == "Male"])/len(salary_data['Gender'])*100, 2)
+share_female = round(len(salary_data[salary_data['Gender'] == "Female"])/len(salary_data['Gender'])*100,2)
+
+# arithmetic mean (total, male and female):
+arith_mean_total = round(sum(salary_data['Salary'])/(len(salary_data['Salary'])), 2)
+# using library pandas:
+# average_salary = salary_data['Salary'].mean()
+
+arith_mean_male_sum = 0
+arith_mean_male_len = 0
+for index, row in salary_data.iterrows():
+    if row['Gender'] == 'Male':
+        arith_mean_male_len += 1
+        arith_mean_male_sum += row['Salary']
+if arith_mean_male_len > 0:
+    arith_mean_male = arith_mean_male_sum / arith_mean_male_len
+# using library pandas:
+# male_salary_data = salary_data[salary_data['Gender'] == 'Male']
+# average_salary_male = male_salary_data['Salary'].mean()
+
+arith_mean_female_sum = 0
+arith_mean_female_len = 0
+for index, row in salary_data.iterrows():
+    if row['Gender'] == 'Female':
+        arith_mean_female_len += 1
+        arith_mean_female_sum += row['Salary']
+if arith_mean_female_len > 0:
+    arith_mean_female = arith_mean_female_sum / arith_mean_female_len
+# using library pandas:
+# female_salary_data = salary_data[salary_data['Gender'] == 'Female']
+# average_salary_female = female_salary_data['Salary'].mean()
+
+
+# arithmetic mean (education level - total, male and female):
+bachelor_male_salaries = salary_data[(salary_data['Gender'] == 'Male') & (salary_data['Education Level'] == "Bachelor's")]['Salary']
+bachelor_female_salaries = salary_data[(salary_data['Gender'] == 'Female') & (salary_data['Education Level'] == "Bachelor's")]['Salary']
+bachelor_total_salaries = salary_data[salary_data['Education Level'] == "Bachelor's"]['Salary']
+master_male_salaries = salary_data[(salary_data['Gender'] == 'Male') & (salary_data['Education Level'] == "Master's")]['Salary']
+master_female_salaries = salary_data[(salary_data['Gender'] == 'Female') & (salary_data['Education Level'] == "Master's")]['Salary']
+master_total_salaries = salary_data[salary_data['Education Level'] == "Master's"]['Salary']
+phd_male_salaries = salary_data[(salary_data['Gender'] == 'Male') & (salary_data['Education Level'] == "PhD")]['Salary']
+phd_female_salaries = salary_data[(salary_data['Gender'] == 'Female') & (salary_data['Education Level'] == "PhD")]['Salary']
+phd_total_salaries = salary_data[salary_data['Education Level'] == "PhD"]['Salary']
+
+
+arith_mean_bachelor_male = np.mean(bachelor_male_salaries)
+arith_mean_bachelor_female = np.mean(bachelor_female_salaries)
+arith_mean_bachelor_total = np.mean(bachelor_total_salaries)
+arith_mean_master_male = np.mean(master_male_salaries)
+arith_mean_master_female = np.mean(master_female_salaries)
+arith_mean_master_total = np.mean(master_total_salaries)
+arith_mean_phd_male = np.mean(phd_male_salaries)
+arith_mean_phd_female = np.mean(phd_female_salaries)
+arith_mean_phd_total = np.mean(phd_total_salaries)
+
+
+# median (total, male and female):
+salaries = salary_data['Salary'].values
+sorted_salaries = np.sort(salaries)
+n_total = len(sorted_salaries)
+if n_total % 2 == 0:
+    median1 = sorted_salaries[n_total//2]
+    median2 = sorted_salaries[n_total//2 - 1]
+    median_total = (median1 + median2)/2
+else:
+    median_total = sorted_salaries[n_total//2]
+# using library numpy:
+# median_total = np.median(salaries)
+
+male_salaries = salary_data[salary_data['Gender'] == 'Male']['Salary']
+sorted_male_salaries = male_salaries.sort_values()
+n_male = len(sorted_male_salaries)
+if n_male % 2 == 0:
+    median1 = sorted_male_salaries.iloc[n_male // 2]
+    median2 = sorted_male_salaries.iloc[n_male // 2 - 1]
+    median_male = (median1 + median2) / 2
+else:
+    median_male = sorted_male_salaries.iloc[n_male // 2]
+# using library numpy:
+# male_salaries = salary_data[salary_data['Gender'] == 'Male']['Salary']
+# median_male = np.median(male_salaries)
+
+female_salaries = salary_data[salary_data['Gender'] == 'Female']['Salary']
+sorted_female_salaries = female_salaries.sort_values()
+n_female = len(sorted_female_salaries)
+if n_female % 2 == 0:
+    median1 = sorted_female_salaries.iloc[n_female // 2]
+    median2 = sorted_female_salaries.iloc[n_female // 2 - 1]
+    median_female = (median1 + median2) / 2
+else:
+    median_female = sorted_female_salaries.iloc[n_female // 2]
+# using library numpy:
+# female_salaries = salary_data[salary_data['Gender'] == 'Female']['Salary']
+# median_female = np.median(female_salaries)
+
+
+# median (education level - total, male and female):
+median_bachelor_male = np.median(bachelor_male_salaries)
+median_bachelor_female = np.median(bachelor_female_salaries)
+median_bachelor_total = np.median(bachelor_total_salaries)
+median_master_male = np.median(master_male_salaries)
+median_master_female = np.median(master_female_salaries)
+median_master_total = np.median(master_total_salaries)
+median_phd_male = np.median(phd_male_salaries)
+median_phd_female = np.median(phd_female_salaries)
+median_phd_total = np.median(phd_total_salaries)
+
+
+# trimmed mean (total, male and female):
+from scipy.stats import trim_mean
+trimmed_mean_total = trim_mean(salaries, proportiontocut=0.2)
+
+trimmed_mean_male = trim_mean(male_salaries, proportiontocut=0.2)
+
+trimmed_mean_female = trim_mean(female_salaries, proportiontocut=0.2)
+# without using a library:
+# sorted_salaries = sorted(salaries)
+# trim_count = int(len(sorted_salaries) * 0.2)
+# trimmed_data = sorted_salaries[trim_count:-trim_count]
+# trimmed_mean_total = sum(trimmed_data) / len(trimmed_data)
+
+
+# trimmed mean (education level - total, male and female):
+trimmed_mean_bachelor_male = trim_mean(bachelor_male_salaries, proportiontocut=0.2)
+trimmed_mean_bachelor_female = trim_mean(bachelor_female_salaries, proportiontocut=0.2)
+trimmed_mean_bachelor_total = trim_mean(bachelor_total_salaries, proportiontocut=0.2)
+trimmed_mean_master_male = trim_mean(master_male_salaries, proportiontocut=0.2)
+trimmed_mean_master_female = trim_mean(master_female_salaries, proportiontocut=0.2)
+trimmed_mean_master_total = trim_mean(master_total_salaries, proportiontocut=0.2)
+trimmed_mean_phd_male = trim_mean(phd_male_salaries, proportiontocut=0.2)
+trimmed_mean_phd_female = trim_mean(phd_female_salaries, proportiontocut=0.2)
+trimmed_mean_phd_total = trim_mean(phd_total_salaries, proportiontocut=0.2)
+
+
+# Variance (total, male and female):
+variance_total = round(statistics.variance(salaries), 1)
+
+variance_male = round(statistics.variance(male_salaries), 1)
+
+variance_female = round(statistics.variance(female_salaries), 1)
+# without using library statistics:
+# difference_squared_total = [(i-arith_mean_total)**2 for i in salaries]
+# sum_difference_squared_total = sum(difference_squared_total)
+# variance_total = sum_difference_squared_total/n_total
+
+
+# Standard deviation (total, male and female):
+standard_deviation_total = round((variance_total**0.5), 2)
+
+standard_deviation_male = round((variance_male**0.5), 2)
+
+standard_deviation_female = round((variance_female**0.5), 2)
+# using library statistics:
+# standard_deviation_total = statistics.stdev(salaries)
+
+
+# sample standard deviation (total, male and female):
+sample_std_dev_total = statistics.pstdev(salaries)
+
+sample_std_dev_male = statistics.pstdev(male_salaries)
+
+sample_std_dev_female = statistics.pstdev(female_salaries)
+# without using library statistics:
+# difference_squared_total = [(i-arith_mean_total)**2 for i in salaries]
+# sum_difference_squared_total = sum(difference_squared_total)
+# variance_sample_std_dev_total = sum_difference_squared_total/(n_total-1)
+# sample_std_dev_m = variance_sample_std_dev_total**0.5
+
+
+# standard error (total, male and female):
+standard_error_total = sample_std_dev_total/((n_total-1)**0.5)
+
+standard_error_male = sample_std_dev_male/((n_male-1)**0.5)
+
+standard_error_female = sample_std_dev_female/((n_female-1)**0.5)
+# using library scipy:
+# from scipy.stats import sem
+# standard_error_total = sem(salaries)
+
+
+# variance (education level):
+bachelor_salaries = salary_data[salary_data['Education Level'] == "Bachelor's"]['Salary']
+variance_bachelor = statistics.variance(bachelor_salaries)
+
+master_salaries = salary_data[salary_data['Education Level'] == "Master's"]['Salary']
+variance_master = statistics.variance(master_salaries)
+
+phd_salaries = salary_data[salary_data['Education Level'] == "PhD"]['Salary']
+variance_phd = statistics.variance(phd_salaries)
+
+
+# standard deviation (education level):
+sample_std_dev_bachelor = statistics.pstdev(bachelor_salaries)
+
+sample_std_dev_master = statistics.pstdev(master_salaries)
+
+sample_std_dev_phd = statistics.pstdev(phd_salaries)
+
+
+# standard error (education level):
+from scipy.stats import sem
+standard_error_bachelor = sem(bachelor_salaries)
+
+standard_error_master = sem(master_salaries)
+
+standard_error_phd = sem(phd_salaries)
+
+
+# confidence interval (total, male and female):
+from scipy.stats import t
+confidence_level = 0.95
+df_total = n_total - 1
+confidence_intervall_total = t.interval(confidence_level, df_total, loc=arith_mean_total, scale=standard_deviation_total/np.sqrt(n_total))
+
+df_male = n_male - 1
+confidence_intervall_male = t.interval(confidence_level, df_male, loc=arith_mean_male, scale=standard_deviation_male/np.sqrt(n_male))
+
+df_female = n_female - 1
+confidence_intervall_female = t.interval(confidence_level, df_female, loc=arith_mean_female, scale=standard_deviation_female/np.sqrt(n_female))
+# without using library scipy:
+# confidence_intervall_total = 1.96*standard_error_total
+# ci_upper_limit = arith_mean_total + confidence_intervall_total
+# ci_lower_limit = arith_mean_total - confidence_intervall_total
+
+
+# interquartile range (total, male and female):
+q1_total = np.percentile(salaries, 25)
+q3_total = np.percentile(salaries, 75)
+iqr_total = q3_total-q1_total
+
+q1_male = np.percentile(male_salaries, 25)
+q3_male = np.percentile(male_salaries, 75)
+iqr_male = q3_male-q1_male
+
+q1_female = np.percentile(female_salaries, 25)
+q3_female = np.percentile(female_salaries, 75)
+iqr_female = q3_female-q1_female
+
+
+# covariance (years of experience vs. salary):
+experience = salary_data['Years of Experience'].values
+arith_mean_experience = sum(experience)/len(experience)
+covariance_exp_sal = np.cov(experience, salaries)[0, 1]
+
+# using library numpy:
+# diff_experience = experience - arith_mean_experience
+# diff_salary = salaries - arith_mean_total
+# covariance_experience_salary = np.mean(diff_experience * diff_salary)
+
+
+# correlation (years of experience vs. salary):
+standard_deviation_experience = statistics.stdev(experience)
+correlation_exp_sal = covariance_exp_sal/(standard_deviation_total*standard_deviation_experience)
+# using library scipy:
+# from scipy.stats import pearsonr
+# correlation_exp_sal = pearsonr(salaries, experience)
+
+
+# probability density function (total):
+from scipy.stats import norm
+pdf_values = norm.pdf(salaries, loc=arith_mean_total, scale=standard_deviation_total)
